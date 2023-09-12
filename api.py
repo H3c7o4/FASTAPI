68aec52dddb871f7665396e426c3c712255a0130
H3c7o4 <hectorvladitok@gmail.com>
2023-09-12 16:48:00 +0200

    Create a Todo list with a fake database

--- api.py
+++ api.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+from typing import Optional
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
+
+
+class CoordIn(BaseModel):
+    password: str
+    lat: float
+    lon: float
+    zoom: Optional[int] = None
+
+class CoordOut(BaseModel):
+    lat: float
+    lon: float
+    zoom: Optional[int] = None
 
 app = FastAPI()
 
@@ -12,8 +27,12 @@ async def get_component(component_id: int):
     return {'component_id': component_id}
 
 @app.get('/component/')
-async def read_component(number: int, text: str):
+async def read_component(number: int, text: Optional[str]):
     return {'number': number, 'text': text}
+
+@app.post('/postion/', response_model=CoordOut)
+async def make_position(coord: CoordIn):
+    return coord #CoordOut(lat=coord.lat, lon=coord.lon, zoom=coord.zoom)
 
 
 if __name__ == '__main__':
